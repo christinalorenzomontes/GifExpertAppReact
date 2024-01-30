11de9b1745e8f71f148f8e152c3e33694cf2a6ec
Christina Lorenzo <christinalorenzo@gmail.com>
2024-01-30 16:13:05 +0100

    add some validations

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,31 +1,34 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['Sailor Moon']);
 
-  const [categories, setCategories] = useState([ 'Sailor Moon'])
-  
-  const onAddCategory = ( newCategory) => {
-    console.log(newCategory);
+  const onAddCategory = (newCategory) => {
+    const lowercaseCategory = newCategory.toLowerCase();
 
-    setCategories([newCategory, ...categories])
-  }
+    if (categories.map(category => category.toLowerCase()).includes(lowercaseCategory)) return;
+
+    setCategories([lowercaseCategory, ...categories]);
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
   return (
     <>
       <h1>GifExpertApp</h1>
 
-      <AddCategory 
-        // setCategories={ setCategories }
-        onNewCategory={ (event) => onAddCategory(event) }
+      <AddCategory
+        onNewCategory={(event) => onAddCategory(event)}
       />
 
       <ol>
-        { categories.map( category => {
-            return <li key={ category }>{ category }</li>
-        }) }
-        
+        {categories.map((category) => (
+          <li key={category}>{capitalizeFirstLetter(category)}</li>
+        ))}
       </ol>
     </>
   );
-}
+};
