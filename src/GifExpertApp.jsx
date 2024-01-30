1f4f3148adc4a885a5a645312997d5ed32ae4d34
Christina Lorenzo <christinalorenzo@gmail.com>
2024-01-30 15:36:45 +0100

    change props

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,15 +5,20 @@ export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState([ 'Sailor Moon'])
   
-  const onAddCategory = () => {
-    setCategories(['Sakura', ...categories])
+  const onAddCategory = ( newCategory) => {
+    console.log(newCategory);
+
+    setCategories([newCategory, ...categories])
   }
 
   return (
     <>
       <h1>GifExpertApp</h1>
 
-      <AddCategory setCategories={ setCategories } />
+      <AddCategory 
+        // setCategories={ setCategories }
+        onNewCategory={ (event) => onAddCategory(event) }
+      />
 
       <ol>
         { categories.map( category => {
