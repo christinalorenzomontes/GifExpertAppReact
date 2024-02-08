7c79f4c81aae4384326a52c58b38a5c0500f3fdb
Christina Lorenzo <christinalorenzo@gmail.com>
2024-02-08 15:48:46 +0100

    add testing in AddCategory component

---
+++ tests/components/AddCategory.test.jsx
@@ -0,0 +1,20 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { AddCategory } from "../../src/components/AddCategory"
+
+
+
+describe('Pruebas en <AddCategory />', () => {
+
+  test('debe cambiar el valor de la caja de texto', () => {
+
+    render( <AddCategory onNewCategory={ () => {} } /> );
+    const input = screen.getByRole('textbox');
+
+    fireEvent.input( input, { target: { value: 'Sailor Moon' } });
+
+    expect( input.value ).toBe('Sailor Moon');
+    screen.debug();
+
+  });
+
+});
